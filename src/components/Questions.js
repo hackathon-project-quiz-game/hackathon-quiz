8279164c708ee8e3cfb665775a4f0431a5d864f6
only1finesse <72461240+only1finesse@users.noreply.github.com>
2022-03-18 12:58:54 -0700

    adds styles to question component

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,17 +3,23 @@ import React from 'react'
 
 
 
-
 function Questions(props) {
+    const buttonClass = props.correctAnswer ? `bg-green-300` : `bg-red-400`
+
     return (
-        <div>
-            <div className='question-section'>
-                <div className='question-text'>{props.question.title}</div>
+        <div className='questions-container w-auto m-5 p-5 text-2xl bg-gray-700 rounded-lg mb-2 flex flex-col items-center justify-center h-4/6 max-h-full'>
+            <div className='question-section text-gray-100'>
+                <div className='question-text text-3xl font-semibold capitalize'>{props.question.title}</div>
             </div>
-            <div className='answer-section'>
+            <div className='answer-section text-gray-300 grid grid-cols-2 mt-10 gap-x-28 gap-y-4 '>
                 {props.question.answers.map((answerOption, index) => (
-                    <div>
-                        <button key={index} onClick={()=>props.handleCorrectAnswerClick(index === props.question.correctAnswer ? true : false)}>{answerOption}</button>
+                    <div className=''>
+                        <button className={`font-medium border-2 border-slate-300 hover:border-slate-500 rounded-sm py-1 px-2 w-44 active:bg-gray-600 ${index === props.question.correctAnswer ? "bg-green-300" : "bg-red-400"}`}
+                            key={index}
+                            onClick={() => props.handleCorrectAnswerClick(index === props.question.correctAnswer ? true : false)}
+                        >
+                            {answerOption}
+                        </button>
                     </div>
                 ))}
                 {/* <button onClick={props.handleClick}>next</button> */}
